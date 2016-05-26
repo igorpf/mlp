6a356ed03b7c460613d5a5adc9025f08f540ae2f
Igor Ferreira <ipferreira@inf.ufrgs.br>
2016-05-26 19:26:15 -0300

    Projeto básico de interface

--- src/main/java/com/mlp/FXMLController.java
+++ src/main/java/com/mlp/FXMLController.java
@@ -6,20 +6,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class FXMLController implements Initializable {
     
     @FXML
-    private Label label;
+    private GridPane mainGrid;
+    
+    @FXML
+    private GridPane nextGrid;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        System.out.println(mainGrid.getHeight());
     }    
 }
