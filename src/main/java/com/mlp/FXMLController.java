6e64c95c8838c2fefe7d62b9f174fa0eac026acd
Igor Ferreira <ipferreira@inf.ufrgs.br>
2016-05-27 20:12:07 -0300

    Preenchimento do quadro com retângulos

--- src/main/java/com/mlp/FXMLController.java
+++ src/main/java/com/mlp/FXMLController.java
@@ -7,21 +7,51 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
     private GridPane mainGrid;
-    
+
     @FXML
     private GridPane nextGrid;
     
+    private Rectangle[][] mainBoard;
+    
+    private Rectangle[][] nextBoard;
     @FXML
     private void handleButtonAction(ActionEvent event) {
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println(mainGrid.getHeight());
-    }    
+        int columns=mainGrid.getColumnConstraints().size();
+        int rows=mainGrid.getRowConstraints().size();
+        mainBoard=new Rectangle[rows][columns];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+
+                mainBoard[i][j] = new Rectangle(25, 25);
+                mainBoard[i][j].setFill(Color.TRANSPARENT);
+                mainGrid.add(mainBoard[i][j], j, i);
+            }
+        }
+        columns=nextGrid.getColumnConstraints().size();
+        rows=nextGrid.getRowConstraints().size();
+        nextBoard=new Rectangle[rows][columns];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+
+                nextBoard[i][j] = new Rectangle(25, 25);
+                nextBoard[i][j].setFill(Color.BLACK);
+                nextGrid.add(nextBoard[i][j], j, i);
+            }
+        }
+    
+    }
+    public void clearBoards(){
+        
+    }
 }
