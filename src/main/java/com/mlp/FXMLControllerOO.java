32a0c73e5a2aeb04559eb66b07dfb525ddb7d48c
Thor C. Sanchotene <qwethor_275@hotmail.com>
2016-06-19 17:05:38 -0300

    Programa agora apaga linhas completas e mostra a próxima peça

--- src/main/java/com/mlp/FXMLControllerOO.java
+++ src/main/java/com/mlp/FXMLControllerOO.java
@@ -47,7 +47,9 @@ public class FXMLControllerOO implements Initializable {
     
     public int NEXT_COLUMNS, NEXT_ROWS;
     
-    private boolean hasStarted=false;
+    private boolean hasStarted = false;
+    
+    private int score = 0;
 
     @FXML
     public void newGame(ActionEvent event) {
@@ -58,6 +60,7 @@ public class FXMLControllerOO implements Initializable {
         ));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
+        printNext();
     }
 
     @Override
@@ -71,7 +74,6 @@ public class FXMLControllerOO implements Initializable {
         });
         MAIN_COLUMNS = mainGrid.getColumnConstraints().size();
         MAIN_ROWS = mainGrid.getRowConstraints().size();
-        createTetromino();
         mainBoard = new Rectangle[MAIN_ROWS][MAIN_COLUMNS];
         for (int i = 0; i < MAIN_ROWS; ++i) {
             for (int j = 0; j < MAIN_COLUMNS; ++j) {
@@ -80,66 +82,79 @@ public class FXMLControllerOO implements Initializable {
                 mainGrid.add(mainBoard[i][j], j, i);
             }
         }
+        colourLimit();
+         
         NEXT_COLUMNS = nextGrid.getColumnConstraints().size();
         NEXT_ROWS = nextGrid.getRowConstraints().size();
+        Tetromino tAux = new TetrominoT();
         nextBoard = new Rectangle[NEXT_ROWS][NEXT_COLUMNS];
         for (int i = 0; i < NEXT_ROWS; ++i) {
             for (int j = 0; j < NEXT_COLUMNS; ++j) {
                 nextBoard[i][j] = new Rectangle(25, 25);
-                nextBoard[i][j].setFill(Color.TRANSPARENT);
+                if(tAux.getBlock()[i][j])
+                    nextBoard[i][j].setFill(tAux.getColor());
+                else
+                    nextBoard[i][j].setFill(Color.TRANSPARENT);  
                 nextGrid.add(nextBoard[i][j], j, i);
             }
         }
+        
         board = new int[MAIN_ROWS][MAIN_COLUMNS];
         for (int i = 0; i < MAIN_ROWS; ++i) {
             for (int j = 0; j < MAIN_COLUMNS; ++j) {
                 board[i][j] = 0;
             }
         }
-
+        createTetromino();
+        createTetromino();
     }
     
     public void createTetromino(){
         int n = (int) (Math.random() * 7.0);
+        if(nextTetro != null){
+            currentTetro = new TetrominoJ();
+            currentTetro.setMin(new Point2D(nextTetro.getMin().getX(),nextTetro.getMin().getY()));
+            currentTetro.setMax(new Point2D(nextTetro.getMax().getX(),nextTetro.getMax().getY()));
+            currentTetro.setColor(nextTetro.getColor());
+            boolean[][] matrix = new boolean[4][4];
+            for(int i = 0; i < nextTetro.getBlock().length; i++)
+                for(int j = 0; j < nextTetro.getBlock()[0].length; j++)
+                    matrix[i][j] = nextTetro.getBlock()[i][j];
+            currentTetro.setBlock(matrix);
+        }
+        else
+            n = 0;
         switch(n){
             case 0:
-                currentTetro = new TetrominoT(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
+                nextTetro = new TetrominoT(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
                 break;
             case 1:
-                currentTetro = new TetrominoI(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
+                nextTetro = new TetrominoI(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
                 break;
             case 2:
-                currentTetro = new TetrominoJ(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
+                nextTetro = new TetrominoJ(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
                 break;
             case 3:
-                currentTetro = new TetrominoL(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
+                nextTetro = new TetrominoL(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
                 break;
             case 4:
-                currentTetro = new TetrominoO(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
+                nextTetro = new TetrominoO(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
                 break;
             case 5:
-                currentTetro = new TetrominoS(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
+                nextTetro = new TetrominoS(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
                 break;
             case 6:
             default:
-                currentTetro = new TetrominoZ(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
+                nextTetro = new TetrominoZ(new Point2D(0, MAIN_COLUMNS/2.0 - 2));
         }
+        if(hasStarted)
+            print(currentTetro, 0);
     }
 
-    public void clearBoards() {
-        for (int i = 0; i < MAIN_ROWS; ++i) {
-            for (int j = 0; j < MAIN_COLUMNS; ++j) {
-                mainBoard[i][j].setFill(Color.TRANSPARENT);
-            }
-        }
+    public void clearNextBoard() {
         for (int i = 0; i < NEXT_ROWS; ++i) {
             for (int j = 0; j < NEXT_COLUMNS; ++j) {
                 nextBoard[i][j].setFill(Color.TRANSPARENT);
-            }
-        }
-        for (int i = 0; i < MAIN_ROWS; ++i) {
-            for (int j = 0; j < MAIN_COLUMNS; ++j) {
-                board[i][j] = 0;
             }
         }
     }
@@ -147,13 +162,13 @@ public class FXMLControllerOO implements Initializable {
     public void move(KeyCode key, Tetromino t) {
         if(!hasStarted)
             return;
-        print(t, 0);
-        boolean canMove = true;
+        print(t, 1);
+        boolean canMove = true, flag = true;
         switch (key) {
             case DOWN:
                 if (t.getMax().getX() < MAIN_ROWS - 1) {
-                    for (int i = 0; i < 4; ++i)
-                        for (int j = 0; j < 4; ++j)
+                    for (int i = 0; i < t.getBlock().length; ++i)
+                        for (int j = 0; j < t.getBlock()[0].length; ++j)
                             if(t.getBlock()[i][j])
                                 if(board[(int)t.getMin().getX()+i+1][(int)t.getMin().getY()+j] != 0){
                                     canMove = false;
@@ -167,18 +182,25 @@ public class FXMLControllerOO implements Initializable {
                 }
                 
                 if(!canMove || t.getMax().getX() >= MAIN_ROWS - 1){ // para a peça: mapeia pra board
-                    for (int i = 0; i < 4; ++i)
-                        for (int j = 0; j < 4; ++j)
+                    for (int i = 0; i < t.getBlock().length; ++i)
+                        for (int j = 0; j < t.getBlock()[0].length; ++j)
                             if (t.getBlock()[i][j])
-                                board[(int)t.getMin().getX() + i][(int)t.getMin().getY() + j] = 1;          
-                    print(t, 0);
+                                board[(int)t.getMin().getX() + i][(int)t.getMin().getY() + j] = Functions.colorToInt(t.getColor());      
                     createTetromino();
+                    printNext();
+                    if(deleteRows())
+                        flag = false;
+                    for(int j = 0; j < MAIN_COLUMNS; j++)
+                        if(board[3][j] > 0)
+                            gameOver();
+                    score += 3;
+                    System.out.println(score);
                 }
                 break;
             case LEFT:
                 if (t.getMin().getY() > 0) {
-                    for (int i = 0; i < 4; ++i)
-                        for (int j = 0; j < 4; ++j)
+                    for (int i = 0; i < t.getBlock().length; ++i)
+                        for (int j = 0; j < t.getBlock()[0].length; ++j)
                             if(t.getBlock()[i][j])
                                 if(board[(int)t.getMin().getX()+i][(int)t.getMin().getY()+j-1] != 0)
                                     canMove = false;
@@ -189,21 +211,21 @@ public class FXMLControllerOO implements Initializable {
                     }
 
                 }
-                for (int i = 0; i < t.getBlock()[0].length; ++i) {
+                for (int i = 0; i < t.getBlock().length; ++i) {
                     if (t.getBlock()[i][0]) {
                         canMove = false;
                     }
                 }
                 if(canMove){
-                    for (int i = 0; i < 4; ++i)
-                        for (int j = 0; j < 4; ++j)
+                    for (int i = 0; i < t.getBlock().length; ++i)
+                        for (int j = 0; j < t.getBlock()[0].length; ++j)
                             if(t.getBlock()[i][j])
                                 if(board[(int)t.getMin().getX()+i][(int)t.getMin().getY()+j-1] != 0)
                                     canMove = false;
                 }
                 
                 if (canMove) {
-                    for (int i = 0; i < t.getBlock()[0].length; ++i) {
+                    for (int i = 0; i < t.getBlock().length; ++i) {
                         for (int j = 1; j < t.getBlock()[0].length; ++j) {
                             t.getBlock()[i][j - 1] = t.getBlock()[i][j];
                             t.getBlock()[i][j]=false;
@@ -213,8 +235,8 @@ public class FXMLControllerOO implements Initializable {
                 break;
             case RIGHT:
                 if (t.getMax().getY() < MAIN_COLUMNS - 1) {
-                    for (int i = 0; i < 4; ++i)
-                        for (int j = 0; j < 4; ++j)
+                    for (int i = 0; i < t.getBlock().length; ++i)
+                        for (int j = 0; j < t.getBlock()[0].length; ++j)
                             if(t.getBlock()[i][j])
                                 if(board[(int)t.getMin().getX()+i][(int)t.getMin().getY()+j+1] != 0)
                                     canMove = false;
@@ -224,20 +246,20 @@ public class FXMLControllerOO implements Initializable {
                         canMove = false;
                     }
                 }
-                for (int i = 0; i < t.getBlock()[0].length; ++i) {
+                for (int i = 0; i < t.getBlock().length; ++i) {
                     if (t.getBlock()[i][3]) {
                         canMove = false;
                     }
                 }
                 if(canMove){
-                    for (int i = 0; i < 4; ++i)
-                        for (int j = 0; j < 4; ++j)
+                    for (int i = 0; i < t.getBlock().length; ++i)
+                        for (int j = 0; j < t.getBlock()[0].length; ++j)
                             if(t.getBlock()[i][j])
                                 if(board[(int)t.getMin().getX()+i][(int)t.getMin().getY()+j+1] != 0)
                                     canMove = false;
                 }
                 if (canMove) {
-                    for (int i = 0; i <t.getBlock()[0].length ; ++i) {
+                    for (int i = 0; i < t.getBlock().length ; ++i) {
                         for (int j = t.getBlock()[0].length-2;j>=0 ; --j) {
                             t.getBlock()[i][j+1] = t.getBlock()[i][j];
                             t.getBlock()[i][j]=false;
@@ -247,38 +269,88 @@ public class FXMLControllerOO implements Initializable {
                 }
                 break;
             case R: // sentido anti-horário
-                if(Functions.rotate(currentTetro,2,board) != null)
-                    currentTetro.setBlock(Functions.rotate(currentTetro,1,board));
+                currentTetro.setBlock(Functions.rotate(currentTetro,1,board));
                 break;
             case T: // sentido horário
-                if(Functions.rotate(currentTetro,2,board) != null)
-                    currentTetro.setBlock(Functions.rotate(currentTetro,2,board));
+                currentTetro.setBlock(Functions.rotate(currentTetro,2,board));
                 break;
                 
             default:
                 break;
         }
-        
-        print(t, 1);
+        if(flag)
+            print(t, 0);
     }
 
-    public void print(Tetromino t, int apagar) {
+    public void print(Tetromino t, int erase) {
 
         for (int i = (int) t.getMin().getX(); i <= t.getMax().getX(); ++i) {
             for (int j = (int) t.getMin().getY(); j <= t.getMax().getY(); ++j) {
                 int indexX = i - (int) t.getMin().getX();
                 int indexY = j - (int) t.getMin().getY();
                 if (t.getBlock()[indexX][indexY]) {
-                    if(apagar == 1){
+                    if(erase == 0)
                         mainBoard[i][j].setFill(t.getColor());
-                        board[i][j] = 1;
-                    }
                     else{
                         mainBoard[i][j].setFill(Color.TRANSPARENT);
-                        board[i][j] = 0;
+                        colourLimit();
                     }
+                        
                 }
             }
         }
     }
+    
+    public void printNext(){
+        clearNextBoard();
+        for (int i = 0 ; i < nextTetro.getBlock().length; ++i) {
+            for (int j = 0; j < nextTetro.getBlock()[0].length; ++j) {
+                if (nextTetro.getBlock()[i][j])
+                    nextBoard[i][j].setFill(nextTetro.getColor());       
+            }
+        }
+    }
+    
+    public boolean deleteRows(){
+        boolean flag, flagExit = false;
+        int points = 10;
+        for(int i = 0; i < MAIN_ROWS; i++){
+            flag = true;
+            for(int j = 0; j < MAIN_COLUMNS; j++)
+                if(board[i][j] == 0)
+                    flag = false;
+            if(flag){
+                score += points;
+                points *= 2;
+                for(int j = 0; j < MAIN_COLUMNS; j++)
+                    board[i][j] = 0;
+                for(int k = i; k > 0; k--)
+                    for(int j = 0; j < MAIN_COLUMNS; j++)
+                        board[k][j] = board[k-1][j];
+                printBoard(i);
+                flagExit = true;
+            }
+        }
+        return flagExit;
+        
+    }
+    
+    public void printBoard(int i){
+        System.out.println(score);
+        for(int k = i; k >= 0; k--)
+            for(int j = 0; j < MAIN_COLUMNS; j++)
+                mainBoard[k][j].setFill(Functions.intToColor(board[k][j]));
+        colourLimit();
+    }
+    
+    public void colourLimit(){
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < MAIN_COLUMNS; j++)
+                mainBoard[i][j].setFill(Color.LIGHTGRAY);
+    }
+    
+    public void gameOver(){
+        System.exit(0);
+    }
+            
 }
--- src/main/java/com/mlp/oo/functions/Functions.java
+++ src/main/java/com/mlp/oo/functions/Functions.java
@@ -1,21 +1,66 @@
 package com.mlp.oo.functions;
 
 import com.mlp.oo.entities.Tetromino;
-import com.mlp.oo.entities.TetrominoJ;
+import javafx.scene.paint.Color;
 
 public class Functions {
+    
+    public static Color intToColor(int n){
+        Color c = Color.TRANSPARENT;
+        switch(n){
+            case 0:
+                c = Color.WHITE;
+                break;
+            case 1:
+                c = Color.STEELBLUE;
+                break;
+            case 2:
+                c = Color.HOTPINK;
+                break;
+            case 3:
+                c = Color.ORANGE;
+                break;
+            case 4:
+                c = Color.GOLD;
+                break;
+            case 5:
+                c = Color.RED;
+                break;
+            case 6:
+                c = Color.DARKMAGENTA;
+                break;
+            case 7:
+                c = Color.DARKGREEN;
+                break;    
+        }
+        return c;
+    }
+    
+    public static int colorToInt(Color c){
+        if(c.equals(Color.STEELBLUE))
+            return 1;
+        if(c.equals(Color.HOTPINK))
+            return 2;
+        if(c.equals(Color.ORANGE))
+            return 3;
+        if(c.equals(Color.GOLD))
+            return 4;
+        if(c.equals(Color.RED))
+            return 5;
+        if(c.equals(Color.DARKMAGENTA))
+            return 6;
+        if(c.equals(Color.DARKGREEN))
+            return 7;
+        return 0;
+    }
             
-    public static boolean[][] rotate(Tetromino peca, int sentido, int[][] map){
+    public static boolean[][] rotate(Tetromino t, int way, int[][] map){
         int i, j, counter = 0;
-        boolean canRotate = true, canMoveDown = true, flag = true;
-        Tetromino tetroAux = new TetrominoJ();
-        tetroAux.setBlock(peca.getBlock());
-        tetroAux.setMax(peca.getMax());
-        tetroAux.setMin(peca.getMin());
-        boolean[][] matrix = tetroAux.getBlock();
-        int rows = matrix.length;
-        int columns = matrix[0].length;
-        boolean matrixAux[][] = new boolean[rows][columns];
+        boolean canRotate = true, canMoveDown = true, flag;
+        final int rows = t.getBlock().length;
+        final int columns = t.getBlock()[0].length;
+        boolean[][] matrix = new boolean[rows][columns];
+        boolean[][] matrixAux = new boolean[rows][columns];
         boolean[][] matrixRotated = new boolean[rows][columns];
         int minX = columns - 1, minY = rows - 1, maxX = 0, maxY = 0;
         
@@ -23,9 +68,10 @@ public class Functions {
         for(i = 0; i < rows; i++)
             for(j = 0; j < columns; j++){
                 matrixRotated[i][j] = false;
+                matrix[i][j] = t.getBlock()[i][j];
             }
         
-        if(sentido == 1){ // sentido anti-horário
+        if(way == 1){ // sentido anti-horário
             // obtém a matriz transposta
             for(i = 0; i < rows; i++)
                 for(j = 0; j < columns; j++)
@@ -64,7 +110,7 @@ public class Functions {
             counter++;
         }
         
-        if(sentido == 2){ // sentido horário
+        if(way == 2){ // sentido horário
             // obtém a matriz transposta
             for(i = minY; i <= maxY; i++)
                 for(j = minX; j <= maxX; j++)
@@ -73,8 +119,7 @@ public class Functions {
         else
             matrixRotated = matrixAux;
         
-        tetroAux.setBlock(matrixRotated);
-        
+        // desce a peça dentro do bounding box
         while(canMoveDown){
             canMoveDown = false;
             flag = true;
@@ -92,14 +137,16 @@ public class Functions {
                 canMoveDown = true;
             }
         }
+        
+        // determina se pode virar
         for(i = 0; i < 4; i++)
             for(j = 0; j < 4; j++)
                 if(matrixRotated[i][j])
-                    if (map[(int)peca.getMin().getX()+i][(int)peca.getMin().getY()+j] != 0)
+                    if (map[(int)t.getMin().getX()+i][(int)t.getMin().getY()+j] != 0)
                         canRotate = false;
                 
         if(!canRotate)
-            return null;
+            return t.getBlock();
         
         return matrixRotated;
     }
